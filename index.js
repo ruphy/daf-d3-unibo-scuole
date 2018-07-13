b41fa26faf5de385c617daf4b81f56b786928b57
esterpantaleo <esterpantaleo@gmail.com>
2018-07-13 11:40:48 +0200

    improving ux

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var legendData = [
 	property: "p_neutral",
 	name: "Le Scuole e i Corsi di Laurea",
 	index: 0,
-	description: "In questa pagina puoi esplorare le diverse Scuole da cui è composta l'Università di Bologna, ognuna rappresentata da una icona diversa (la provetta ad esempio rappresenta la Scuola di Scienze, le cuffie la Scuola di Lingue e Letterature, Traduzione e Interpretazione).</br>I cerchi intorno alle icone sono i Corsi di ciascuna Scuola.</br><span style='color:red;'>",
+	description: "In questa pagina puoi esplorare le diverse Scuole da cui è composta l'Università di Bologna, ognuna rappresentata da un'icona diversa (la provetta ad esempio rappresenta la Scuola di Scienze, le cuffie la Scuola di Lingue e Letterature, Traduzione e Interpretazione).</br>I cerchi intorno alle icone sono i Corsi di ciascuna Scuola.</br><span style='color:red;'>",
 	title: "Esplora le diverse Scuole dell'Università di Bologna"
     },{
 	property: "p_lavorano",
@@ -30,7 +30,7 @@ var legendData = [
 	property: "p_laureatiincorso",
 	name: "Laureati in corso",
 	index: 4,
-	description: "Il grafico mostra la percentuale di studenti immatricolati nell'a.a. 2013/14 (lauree triennali), 2014/15 (lauree magistrali), 2011/12 (lauree magistrali a ciclo unico di durata 5 anni), 2010/11 (lauree magistrali a ciclo unico di durata 6 anni). <span style='color:red;'>",
+	description: "Il grafico mostra la percentuale di studenti immatricolati nell'a.a. 2013/14 (lauree triennali), 2014/15 (lauree magistrali), 2011/12 (lauree magistrali a ciclo unico di durata 5 anni), 2010/11 (lauree magistrali a ciclo unico di durata 6 anni) laureati in corso nel 2016. <span style='color:red;'>",
 	title: "Percentuale di studenti laureati in corso"
     },{
 	property: "p_esperienzaestero",
@@ -40,6 +40,8 @@ var legendData = [
 	title: "Percentuale di laureati con esperienza all'estero"
     }];
 //    { property: "p_soddisfatti", name: "Soddisfatti", index: 6},
+
+var clickedButton = "none";
 
 var buttons = d3.select('#leftDiv')
     .selectAll('input')
@@ -51,11 +53,11 @@ var buttons = d3.select('#leftDiv')
     .attr('class', 'button')
     .attr('value', (b) => b.name);
 
-var width = 1400,
-    height = 1250,
-    padding = 0, // separation between same-color circles
-    clusterPadding = 30, // separation between different-color circles
-    maxRadius = height*0.03;
+var width = 800,
+    height = 700,
+    padding = 2, // separation between same-color circles
+    clusterPadding = 20, // separation between different-color circles
+    maxRadius = height*0.028;
 
 var radiusScale = d3.scaleLinear()
     .domain([0,1])
@@ -96,43 +98,47 @@ d3.csv("data/merged_mod.csv", data => {
         .html(instructions);
     
     d3.selectAll('.button')
-       .on('click',(b) => {
+	.on('click',(b) => {
+	    clickedButton = b;
+	    console.log(clickedButton)
+	    //remove tooltip
+	    d3.select('.tooltip').remove();
 
-	   //update header
-	   d3.select('#header')
-	       .innerHtml = "";
-	   d3.select('#header')
-	       .html(b.title);
-	   //update chart
-	   d3.select('#svgChart')
-               .remove();
-	   update(data, b.property);
-	   
-	   //update buttons
-	   d3.selectAll('.button')
-               .style('background-color', '#B0B0B0');
-           d3.select('#button_' + b.index)
-               .style('background-color', '#00008B');
-	   
-	   //update description
-	   d3.select('.description')
-               .remove();
-	   d3.select('.instructions')
-	       .remove();
-	   d3.select('#leftDiv')
-	       .append('div')
-	       .attr('class', 'description')
-	       .html(b.description);	   
-	   d3.select('#leftDiv')
-	       .append('div')
-	       .attr('class', 'description instructions')
-	       .html(instructions);
-
-	   d3.select('#svgLegend')
-               .remove();
-           if (b.index !== 0) drawLegend();
-           
-       });
+	    //update header
+	    d3.select('#header')
+		.innerHtml = "";
+	    d3.select('#header')
+		.html(b.title);
+	    //update chart
+	    d3.select('#svgChartDiv')
+		.remove();
+	    update(data, b.property);
+	    
+	    //update buttons
+	    d3.selectAll('.button')
+		.style('background-color', '#B0B0B0');
+            d3.select('#button_' + b.index)
+		.style('background-color', '#00008B');
+	    
+	    //update description
+	    d3.select('.description')
+		.remove();
+	    d3.select('.instructions')
+		.remove();
+	    d3.select('#leftDiv')
+		.append('div')
+		.attr('class', 'description')
+		.html(b.description);	   
+	    d3.select('#leftDiv')
+		.append('div')
+		.attr('class', 'description instructions')
+		.html(instructions);
+	    
+	    d3.select('#svgLegend')
+		.remove();
+            if (b.index !== 0)
+		drawLegend();
+	});
 });
 
 function update(data, property) {
@@ -140,12 +146,13 @@ function update(data, property) {
 
     var svg = d3.select('#rightDiv')
 	.append('div')
-	.attr('id', 'svgChart')
+	.attr('id', 'svgChartDiv')
 	.append('svg')
+	.attr('id', 'svgChart')
 	.attr('height', height)
 	.attr('width', width)
 	.append('g')
-	.attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
+	.attr('transform', 'translate(350,290)');
 
     var nodes = data.map((e) => {
 	// scale radius to fit on the screen
@@ -187,31 +194,13 @@ function update(data, property) {
 	      .on('end', dragended)
 	     )
         // add tooltips to each circle
-        .on('click', d => onCircleClick(d))
-	//.on("mouseover", onCircleMouseover);
-        
+        .on('click', onCircleClick)
+    
     svg.append('text')
+	.attr('class', 'credits')
 	.html("Icone <a href='http://www.freepik.com' title='Freepik'>Freepik</a> <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a> licenza <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0 BY</a>")
-        .attr('transform', 'translate(0,' + height/2 + ')');
-/*
-    d3.select('myPopup')
-        .on('click', () => {
-	    d3.select('myPopup').visibility = 'hidden !important';
-	})
-	
-    d3.select('body')
-	.on('click', () => {
-	    console.log("click on body");
-	    var popup = document.getElementById('myPopup');
-	    popup.visibility = 'hidden !important';
-	});
-    svg.on('click', () => {
-	console.log("click on svg");
+        .attr('transform', 'translate(0,' + 350 + ')');
 
-        var popup = document.getElementById('myPopup');
-        popup.visibility = 'hidden !important';
-    });
-    */
     var images = nodeEnter.filter((e,i) => i<11).append('image')
         .attr('id', (d,i) => 'image_'+i)
         .attr('xlink:href', (d) => d.internazionale)
@@ -309,44 +298,49 @@ function update(data, property) {
 };
 
 function drawLegend() {
-    var legend = d3.select('#legend')
-	.append('svg')
-	.attr('id', 'svgLegend')
-        .attr('height', '150px')
-        .attr('width', '460px')
+    var legend = d3.select('#svgChart')
         .selectAll('g')
 	.data([0.25,0.50,0.75,1], (d) => d)
 	.enter()
-	.append('g');
+	.append('g')
+	.attr('transform', 'translate(530,570)');
     
     legend.append('circle')
-	.attr('cy', 50)
-	.attr('cx', (d, i) => i * 70 + radiusScale(d) * 2 + 30)
+	.attr('cy', 22)
+	.attr('cx', (d, i) => i * 30 + radiusScale(d) * 2 + 20)
 	.attr('r', (d) => radiusScale(d))
-	.attr('fill', 'red')
+	.attr('fill', 'none')
         .attr('stroke', 'black')
 	.attr('stroke-width', 1);
     legend.append('text')
-	.attr('y', 130)
-        .attr('x', (d, i) => i * 70 + radiusScale(d) * 2 + 12)
-	.attr('font-size', '30px')
+	.attr('y', 55)
+        .attr('x', (d, i) => i * 28 + radiusScale(d) * 2 + 8)
+	.attr('font-size', '15px')
 	.html((d) => (d * 100) + '%');
+
+    legend.append('text')
+        .attr('y', 26)
+        .attr('x', -20)
+        .attr('font-size', '10px')
+        .html('Scala');
 };
 
 function onCircleClick(d) {
+    d3.select('.tooltip').remove();
+    
     var div = d3.select('body')
 	.append('div')
 	.attr('class', 'tooltip')
 	.on('click', () => div.remove());
-    console.log("click")
+    
     div.transition()
 	.duration(500)	
 	.style("opacity", 0);
     div.transition()
 	.duration(200)	
 	.style("opacity", 1);
-    div.html(printTooltip(d))
-	.style("left", (d3.event.pageX - 700) + "px")			 
+    div.html(printTooltip(d, clickedButton))
+	.style("left", (d3.event.pageX - 300) + "px")			 
 	.style("top", (d3.event.pageY - 28) + "px");
     
 };
@@ -367,25 +361,28 @@ function parse(a) {
     return Math.round(a) + "%";
 };
 
-function printTooltip(d) {
+function printTooltip(d, b) {
+    console.log(b)
     var text = "Scuola di " + d.major_cat;
     if (d.tipologia !== "Scuola") {
-        text = text +
-	    "<br/><b>" + d.major + "</b></br>" +
-	    "Corso di " + d.tipologia + " (" + d.durata + " anni)</br>" +
-	    "Corso" + (d.internazionale ? " " : " non ") + "internazionale in " + d.lingue + " con titolo" + (d.internazionale_titolo ? " " : " non ") + "internazionale</br></br>" +
-	    "<a title='vai al sito' href=" + d.url + " target='_blank'>vai al sito</a></br></br>" +
-            "Campus: " + d.campus + "</br>" +
-            "Sede didattica: " + d.sededidattica + "</br>" +
+        text = 
+	    "<b><span style='font-size:20px;'>" + d.major + "</span></b> - " + text + "</br>" +
+	    "Corso di " + d.tipologia + " (" + d.durata + " anni) &nbsp;&nbsp;&nbsp; <a title='vai al sito' href=" + d.url + " target='_blank'>vai al sito</a></br>" +
+	    "Corso" + (d.internazionale ? " " : " non ") + "internazionale in " + d.lingue + " con titolo" + (d.internazionale_titolo ? " " : " non ") + "internazionale</br>" +
+            "Campus: " + d.campus + " - " + 
+            "Sede didattica: " + d.sededidattica + " - " +
             "Corso attivo: " + (d.immatricolabile ? "sì" : "no") + "</br>" +
-	    "Accesso: " + d.accesso + "</br></br>" +
-	    "<b>Percentuali riferite ai laureati nell’anno solare 2016</b></br>" +
+	    "Accesso: " + d.accesso +
+	    "<hr style='height:0;'>" +
+	    
 	    "<table style='width:100%'>" +
-	    "<tr><th>Bologna</th><th>Italia</th><th></th></tr>" + 
-            "<tr><td><b>" + parse(100 * d.p_fuoriregione) + "</b></td><td>-</td><td>Studenti internazionali o provenienti da altre regioni</td></tr>" +
-	    "<tr><td><b>" + parse(100 * d.p_esperienza_estero) + "</b></td><td>-</td><td>Laureati con esperienza all'estero</td></tr>" +
-            "<tr><td><b>" + parse(100 * d.p_soddisfatti) + "</b></td><td>" + parse(100 * d.p_soddisfatti_italia) + "</td><td>Laureati soddisfatti dagli studi svolti</td></tr>" +
-            "<tr><td><b>" + parse(100 * d.p_lavorano) + "(" + parse(100 * d.p_studiano) + ")</b></td><td>" + parse(100 * d.p_lavorano_italia) + "("  + parse(100 * d.p_studiano_italia) + ")</td><td>Laureati che lavorano (non lavorano) ma studiano o cercano</td></tr></table>";
+	    "<tr><th>Bologna</th><th>Media Italia</th><th><b>Anno solare 2016</b></br></th></tr>" + 
+            "<tr bgcolor='" + ((b.property==="p_fuoriregione")? "yellow" : "white") + "'><td><b>" + parse(100 * d.p_fuoriregione) + "</b></td><td>-</td><td>Percentuale di studenti internazionali o provenienti da altre regioni</td></tr>" +
+	    "<tr bgcolor='" + ((b.property==="p_esperienzaestero")? "yellow" : "white") + "'><td><b>" + parse(100 * d.p_esperienzaestero) + "</b></td><td>-</td><td>Percentuale di laureati con esperienza all'estero</td></tr>" +
+            "<tr><td><b>" + parse(100 * d.p_soddisfatti) + "</b></td><td>" + parse(100 * d.p_soddisfatti_italia) + "</td><td>Percentuale di laureati soddisfatti dagli studi svolti</td></tr>" +
+            "<tr bgcolor='" + ((b.property==="p_lavorano")? ((d.p_lavorano>=d.p_lavorano_italia)? "green" : "red") : 'white') + "'><td><b>" + parse(100 * d.p_lavorano) + "</b></td><td>" + parse(100 * d.p_lavorano_italia) + "</td><td>Percentuale di laureati che lavorano</td></tr>" +
+	    "<tr bgcolor='" + ((b.property==="p_studiano")? ((d.p_studiano>=d.p_studiano_italia)? "red" : "green") : 'white') + "'><td><b>" + parse(100 * d.p_studiano) + "</b></td><td>" + parse(100 * d.p_studiano_italia) + "</td><td>Percentuale di laureati che non lavorano ma studiano o cercano</td></tr>" +
+	     "<tr bgcolor='" + ((b.property==="p_laureatiincorso")? "yellow" : "white") + "'><td><b>" + parse(100 * d.p_laureatiincorso) + "</b></td><td>-</td><td>Percentuale di studenti laureati in corso</td><tr></table>";
     }
     return text;
 };
